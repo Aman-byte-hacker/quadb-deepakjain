e13b865ded388ab910a9b31233ec23c81a439aca
Aman-byte-hacker <vyasaman21@gmail.com>
2021-09-21 22:36:05 +0100

    solved image problem

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,38 +1,33 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Main = ({ name, type, lang, img }) => {
-    const width = {
-        width: '18rem',
-    }
-    let history = useHistory()
-    const Change = () => {
-        history.push('/summary')
-    }
-    return ( <
-        >
-        <
-        div className = "card"
-        style = { width } >
-        <
-        img src = { img }
-        alt = "x" / > { ' ' } <
-        div className = "card-body" >
-        <
-        h5 className = "card-title" > { name } < /h5>{' '} <
-        div className = "detail" >
-        <
-        p className = "card-text " > type: { type } < /p>{' '} <
-        p className = "card-text" > language: { lang } < /p>{' '} <
-        /div>{' '} <
-        button className = "btn"
-        onClick = { Change } > { ' ' }
-        Go somewhere { ' ' } <
-        /button>{' '} <
-        /div>{' '} <
-        /div>{' '} <
-        />
-    )
-}
+  const width = {
+    width: "18rem",
+  };
+  let history = useHistory();
+  const Change = () => {
+    history.push("/summary");
+  };
+  
+  return (
+    <>
+      <div className="card" style={width}>
+        <img src={img} alt="image" />
+        <div className="card-body">
+          <h5 className="card-title"> {name} </h5>{" "}
+          <div className="detail">
+            <p className="card-text "> type: {type} </p>{" "}
+            <p className="card-text"> language: {lang} </p>{" "}
+          </div>{" "}
+          <button className="btn" onClick={Change}>
+            {" "}
+            Go somewhere{" "}
+          </button>{" "}
+        </div>{" "}
+      </div>{" "}
+    </>
+  );
+};
 
-export default Main
+export default Main;
--- src/Component/home.js
+++ src/Component/home.js
@@ -1,46 +1,58 @@
-import React, { useEffect, useState } from 'react'
-import Main from './Main'
-import Summary from './Summary'
+import React, { useEffect, useState } from "react";
+import Main from "./Main";
+import Summary from "./Summary";
 export const Home = () => {
-    const [data, setdata] = useState([])
-    const [summary, setsummary] = useState([])
+  const [data, setdata] = useState([]);
+  const [summary, setsummary] = useState([]);
 
-    const getdata = async() => {
-        const url = `https://api.tvmaze.com/search/shows?q=all`
-        const responce = await fetch(url)
-        const resjson = await responce.json()
-        setdata(resjson)
-        setsummary(resjson)
-    }
-    useEffect(() => {
-            getdata()
-        }, [])
-        // summary.map((a) => {
-        //     return console.log(a.show.summary)
-        // })
-    return ( <
-        >
-        <
-        div className = "container" > { ' ' } {
-            data.map((cur) => {
-                console.log(cur.show.summary)
-                return ( <
-                    Main key = { cur.id }
-                    name = { cur.show.name }
-                    type = { cur.show.type }
-                    lang = { cur.show.language }
-                    // img = { cur.show.medium }
+  const getdata = async () => {
+    const url = `https://api.tvmaze.com/search/shows?q=all`;
+    const responce = await fetch(url);
+    const resjson = await responce.json();
+    setdata(resjson);
+    setsummary(resjson);
+  };
+  useEffect(() => {
+    getdata();
+  }, []);
+  // summary.map((a) => {
+  //     return console.log(a.show.summary)
+  // })
+  return (
+    <>
+      <div className="container">
+        {" "}
+        {data.map((cur) => {
+          console.log(cur.show.summary);
+            if (cur.show.image){
+                return(
+                    <Main 
+                        key={cur.id}
+                        name={cur.show.name}
+                        type={cur.show.type}
+                        lang={cur.show.language}
+                        img = {cur.show.image.original}
                     />
                 )
-            })
-        } { ' ' } {
-            summary.map((a) => {
-                return <Summary id = { a.id }
-                summary = { a.show.summary }
-                />
-            })
-        } { ' ' } <
-        /div>{' '} <
-        />
-    )
-}
+            }
+            else{
+                return(
+                    <Main
+                        
+                        key={cur.id}
+                        name={cur.show.name}
+                        type={cur.show.type}
+                        lang={cur.show.language}
+                        img="https://static.tvmaze.com/uploads/images/medium_portrait/280/701869.jpg"
+                    />
+                )
+            }
+       
+        })}{" "}
+        {summary.map((a) => {
+          {/* return <Summary id={a.id} summary={a.show.summary} />; */}
+        })}{" "}
+      </div>{" "}
+    </>
+  );
+};
